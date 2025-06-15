cf7c21b152d1ba7baf10d6d8bf8a787194049042
Ostap Skakun <o.skakun@kse.org.ua>
2025-06-15 12:06:04 +0300

    Final Assignment 5 scenario

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -1,16 +1,18 @@
 import requests
 import os
 from dotenv import load_dotenv
-from pprint import pprint
 import pandas as pd
 
 load_dotenv()
 API_KEY = os.getenv("WEATHERAPI_KEY")
-
 BASE_URL = "https://api.weatherapi.com/v1"
 
 resp = requests.get(f"{BASE_URL}/forecast.json?key={API_KEY}&q=Kyiv&days=7")
 # print(resp.status_code)
+
+if (resp.status_code != 200):
+    print("L, API responded with status code " + str(resp.status_code))
+    exit(1)
 
 data = {
     'temp': [],
@@ -25,7 +27,7 @@ for day in resp.json()['forecast']['forecastday']:
 df = pd.DataFrame(data)
 print(f"Max temp in 3 days: {df.loc[0:71, 'temp'].max()}")
 print(f"Min temp in 3 days: {df.loc[0:71, 'temp'].min()}")
-print(f"Avg temp in 3 days (rounded to 2 symbols after coma): {df.loc[0:71, 'temp'].mean().round(2)}")
+print(f"Avg temp in 3 days (rounded to 1-2 symbols after coma): {df.loc[0:71, 'temp'].mean().round(2)}")
 
 wind_avg = df['wind_spread'].mean()
 # print(f"Avg wind: {wind_avg}")
